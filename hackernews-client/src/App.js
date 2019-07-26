f856bf81a60ca8d33cd04c056ea21d9623254a96
Franco Brusatti <fbrusatti@gmail.com>
2019-07-26 00:05:22 -0300

    Add Router to system

--- hackernews-client/src/App.js
+++ hackernews-client/src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
 import LinkListPage from './LinkListPage'
 import CreateLink from './CreateLink'
+import Header from './Header'
 
-
-function App() {
-  return (
-    <div>
-      <CreateLink />
-      <LinkListPage />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='center w85'>
+        <Header />
+        <div className='ph3 pv1 background-gray'>
+          <Switch>
+            <Route exact path='/' component={LinkListPage}/>
+            <Route exact path='/create' component={CreateLink}/>
+          </Switch>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
